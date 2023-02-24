cc49cee4e50339a4bf90b9fde2c114c65f165333
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-24 21:00:29 +0900

    Vuexを使用してストアを作成

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
+import store from './store'
+
 createApp(App)
     .use(router)
+    .use(store)
     .mount('#app')
