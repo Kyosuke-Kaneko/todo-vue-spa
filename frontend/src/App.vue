f2c94996762ddd2fc72b4f0c0a6f6cfd92319705
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-24 13:03:05 +0900

    ナビゲーション・フッタコンポーネント追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
+    <header>
+      <Navbar />
+
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
+      <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
+      <link rel="stylesheet" href="https://hypertext-candy.s3-ap-northeast-1.amazonaws.com/posts/vue-laravel-tutorial/app.css">
+    </header>
     <main>
       <div class="container">
         <RouterView />
+        <!--
+        ルートとマッチしたコンポーネントがここで描画される
+        -->
       </div>
     </main>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav className="navbar">
+    <RouterLink class="navbar__brand" to="/">
+      Vuesplash
+    </RouterLink>
+    <div className="navbar__menu">
+      <div className="navbar__item">
+        <button className="button">
+          <i className="icon ion-md-add"></i>
+          Submit a photo
+        </button>
+      </div>
+      <span className="navbar__item">
+        username
+      </span>
+      <div className="navbar__item">
+        <RouterLink class="button button--link" to="/login">
+          Login / Register
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<!--
+RouterLinkはRouterViewと同様、VueRouterから提供されているコンポーネント
+役割はアンカーリンクとほぼ同じで、HTML的にも<a>要素が描画される
+だが、普通のアンカーリンクと違うのは通常の画面遷移（サーバーサイドへのGETリクエスト）が発生しないこと
+
+RouterLinkで描画したリンクをクリックすると、VueRouterによるコンポーネントの切り替わりが発生
+-->
