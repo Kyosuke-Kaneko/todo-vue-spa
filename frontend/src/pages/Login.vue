157d967e02ef650911e9a8c281a4bffb1342f249
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-24 18:35:11 +0900

    v-modelでログインフォーム作成

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -12,7 +12,23 @@
           @click="tab = 2"
       >Register</li>
     </ul>
-    <div class="panel" v-show="tab === 1">Login Form</div>
+
+    <div class="panel" v-show="tab === 1">
+      <form class="form">
+        <label for="login-email">Email</label>
+        <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+        <!--
+        v-modelは、入力値をデータと同期する
+        -->
+
+        <label for="login-password">Password</label>
+        <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">login</button>
+        </div>
+      </form>
+    </div>
     <div class="panel" v-show="tab === 2">Register Form</div>
     <!--
     v-showディレクティブを使えば、tabが特定の値の時のみ表示される
@@ -22,9 +38,13 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      tab: 1
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
     }
   }
 }
