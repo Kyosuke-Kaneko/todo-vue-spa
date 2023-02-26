69286faeb5b3b5806591cb412b028799c34a7460
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-26 16:56:49 +0900

    ナビゲーションガードを実現

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import store from './store'
+
 // ページコンポーネントをインポートする
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
@@ -8,11 +10,26 @@ import Login from './pages/Login.vue'
 const routes = [
     {
         path: '/',
-        component: PhotoList
+        component: PhotoList,
     },
     {
         path: '/login',
-        component: Login
+        component: Login,
+        beforeEnter (to, from, next) {
+            if (store.getters['auth/check']) {
+                next('/')
+            } else {
+                next()
+            }
+        }
+        // beforeEnterは、定義されたルートにアクセスされてページコンポーネントが切り替わる直前に呼び出される関数
+        // beforeEnterの第一引数のtoはアクセスされようとしているルートオブジェクト、
+        // 第二引数fromはアクセス元のルート、
+        // 第三引数nextはページの遷移先を決める関数
+
+        // next()を引数なしで呼ぶとそのままページコンポーネントが切り替わる
+        // 引数ありでnext()を呼ぶと、切り替わるはずのページコンポーネントは生成されず、引数のページに切り替わり、リダイレクトのようになる
+        // TODO beforeEachを使用して実現できるようにする
     }
 ]
 
